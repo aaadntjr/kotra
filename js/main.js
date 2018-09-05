9ea7a54820ba41df5a7f868d1a15f823e3237b03
Choi woo seok <38950467+aaadntjr@users.noreply.github.com>
2018-09-05 14:27:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ $(window).on('load', function () {
     //카드 위아래 움직이기
     var lifelist_center = ($(".life_list").height() * 0.5);
     var life_center = ($(".life").height() * 0.5);
-//    console.log(lifelist_top);
-//    console.log(life_top);
     
     $(".life_list").css({"top" : life_center - lifelist_center});
      console.log(life_center - lifelist_center);
@@ -26,6 +24,59 @@ $(window).on('load', function () {
     $(".life_list_item").eq(1).click(function(){
         $(".life_list").css({"top" : life_center - lifelist_center});
     });//카드 끝
+    
+    //등장효과
+    $(".header_wrap").addClass("show");
+    
+    
+
+
+    
+    $(window).scroll(function(){
+        var show_iphone = $(".header_wrap").offset().top + ($(".header_wrap").height() * 0.5);
+        var show_life = $(".iphone").offset().top + ($(".iphone").height() * 0.5);
+        var show_gift = $(".life").offset().top + ($(".life").height() * 0.5);
+        var show_rate = $(".gift").offset().top + ($(".gift").height() * 0.5);
+        var show_ft = $(".rate").offset().top + ($(".rate").height() * 0.5);
+        
+        console.log(show_iphone);
+        
+        if($(this).scrollTop() >= show_iphone) {
+            $(".iphone").addClass("show");
+        }
+        
+        if ($(this).scrollTop() >= show_life) {
+            $(".life").addClass("show");
+            $(".life_list").addClass("show");
+            $(".life_txt").addClass("show");  
+        }
+        
+        if ($(this).scrollTop() >= show_gift) {
+            $(".gift").addClass("show");
+            setTimeout(function(){
+                $(".gift_list_item").addClass("show");
+                $(".gift_list_item").eq(0).addClass("gift_show_01");
+                $(".gift_list_item").eq(1).addClass("gift_show_02");
+                $(".gift_list_item").eq(2).addClass("gift_show_03");
+            },500);
+            setTimeout(function(){
+                $(".gift_list_item").eq(0).removeClass("gift_show_01");
+                $(".gift_list_item").eq(1).removeClass("gift_show_02");
+                $(".gift_list_item").eq(2).removeClass("gift_show_03");
+            },3000);
+        }
+        
+        if ($(this).scrollTop() >= show_rate) {
+            $(".rate").addClass("show");
+        }
+        
+        if ($(this).scrollTop() >= show_ft) {
+            $("footer").addClass("show");
+        }
+        
+        return false
+    });/*스크롤 펑션*/
+    
 
 
 });/*onload*/
